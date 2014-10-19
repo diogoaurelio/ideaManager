542a2932ad42f5b91f01f79f01d880c0f2c110bb
diogopaurelio@gmail.com <diogopaurelio@gmail.com>
2014-10-19 20:47:44 +0200

    Added column description:text to ideas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141017190341) do
+ActiveRecord::Schema.define(version: 20141019184317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20141017190341) do
     t.integer  "votes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "users", force: true do |t|
